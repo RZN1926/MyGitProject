87d55113224742f159c6703d4d4c5ba1e362774a
RZN1926 <kalygulovbeka7@gmail.com>
2026-02-20 15:48:58 +0600

    First commit

---
+++ notes.py
@@ -0,0 +1,6 @@
+
+# x = list(map(int, input().split()))      для многозначного ввода в input() числами
+# x = [int(x) for x in input().split()]    для многозначного ввода в input() числами
+# x = input().split()                      для многозначного ввода в input() слов
+# print(x.count(' ') + 1)                  подсчет слов в тексте
+# print(len(x.split()))                    подсчет слов в тексте
